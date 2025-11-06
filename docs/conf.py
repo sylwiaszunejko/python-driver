27ca050c7ecc72008eded5d2ba9efaeac16071fb
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2025-11-06 07:51:10 -0400

    docs: update tags to publish

--- docs/conf.py
+++ docs/conf.py
@@ -10,14 +10,30 @@ import cassandra
 # -- Global variables
 
 # Build documentation for the following tags and branches
-TAGS = ['3.21.0-scylla', '3.22.3-scylla', '3.24.8-scylla', '3.25.4-scylla', '3.25.11-scylla', '3.26.9-scylla', '3.28.0-scylla', '3.28.1-scylla', '3.28.2-scylla']
+TAGS = [
+    '3.21.0-scylla',
+    '3.22.3-scylla',
+    '3.24.8-scylla',
+    '3.25.4-scylla',
+    '3.25.11-scylla',
+    '3.26.9-scylla',
+    '3.28.0-scylla',
+    '3.28.1-scylla',
+    '3.28.2-scylla',
+    '3.29.0-scylla',
+    '3.29.1-scylla',
+    '3.29.2-scylla',
+    '3.29.3-scylla',
+    '3.29.4-scylla',
+    '3.29.5-scylla',
+]
 BRANCHES = ['master']
 # Set the latest version.
-LATEST_VERSION = '3.28.2-scylla'
+LATEST_VERSION = '3.29.5-scylla'
 # Set which versions are not released yet.
 UNSTABLE_VERSIONS = ['master']
 # Set which versions are deprecated
-DEPRECATED_VERSIONS = ['3.21.0-scylla', '3.22.3-scylla', '3.24.8-scylla', '3.25.4-scylla', '3.25.11-scylla', '3.26.9-scylla', '3.28.1-scylla']
+DEPRECATED_VERSIONS = ['3.21.0-scylla', '3.22.3-scylla', '3.24.8-scylla', '3.25.4-scylla', '3.25.11-scylla', '3.26.9-scylla', '3.28.1-scylla', '3.29.1-scylla']
 
 # -- General configuration 
 
